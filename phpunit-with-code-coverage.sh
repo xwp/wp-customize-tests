03681b67d31c08ed77723b218e24e055abad2726
Weston Ruter <weston@xwp.co>
2015-03-07 12:40:37 +1100

    Rename output directory to coverage-html and make push not default

--- phpunit-with-code-coverage.sh
+++ phpunit-with-code-coverage.sh
@@ -10,13 +10,13 @@ if [ ! -e ../.git ] || [ ! -e ../wp-tests-config.php ]; then
 	exit 1
 fi
 
-skip_gh_pages_push=0
+gh_pages_push=0
 while [[ $# > 0 ]]; do
 	key="$1"
 
 	case "$key" in
-		--no-push|--local|--skip-push|-s)
-		skip_gh_pages_push=1
+		--push)
+		gh_pages_push=1
 		;;
 
 		*)
@@ -28,7 +28,7 @@ done
 
 ssh_remote=git@github.com:xwp/wp-customize-tests.git
 self_dir="$(pwd)"
-gh_pages_dir="$self_dir/gh-pages"
+gh_pages_dir="$self_dir/coverage-html"
 
 if [ -e ../phpunit.customize.xml ]; then
 	rm ../phpunit.customize.xml
@@ -40,20 +40,22 @@ cp phpunit.customize.xml ../phpunit.customize.xml
 # Go to wordpress-develop root
 cd ..
 
-if [ ! -e "$gh_pages_dir" ]; then
-	git clone -b gh-pages "$ssh_remote" "$gh_pages_dir"
+if [ ! -e "$gh_pages_dir/.git" ] && [ "$gh_pages_push" == 1 ]; then
+	if [ "$gh_pages_dir" ]; then
+		rm -r "$gh_pages_dir"
+	fi
+	git clone -b gh-pages --single-branch --depth 1 "$ssh_remote" "$gh_pages_dir"
 fi
 
 phpunit -c phpunit.customize.xml --coverage-html="$gh_pages_dir"
 
-if [ "$skip_gh_pages_push" == 0 ]; then
+if [ "$gh_pages_push" == 1 ]; then
 	# Commit and push
 	cd "$gh_pages_dir"
 	git remote set-url origin "$ssh_remote" # SSH so we can use it in Vagrant
 	git config user.name = "PHPUnit"
 	git config user.email = "phpunit@xwp.co"
-	git add -A .
-	git commit --amend -m "Code coverage report"
+	git commit --all --amend -m "Code coverage report"
 	if git push -f origin HEAD:gh-pages; then
 		echo "Code Coverage report now avalable at http://xwp.github.io/wp-customize-tests/"
 	else
